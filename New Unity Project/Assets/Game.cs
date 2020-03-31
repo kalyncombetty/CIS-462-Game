9fb18a5130fbd3f0521a5a1428844a38e04884a8
MikeFredley <36174846+MikeFredley@users.noreply.github.com>
2020-03-31 13:05:15 -0400

    Initial assets folder

---
+++ New Unity Project/Assets/Game.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField]
+    Vector2Int boardSize = new Vector2Int(11, 11);
+
+    [SerializeField]
+    GameBoard board = default;
+
+    void Awake()
+    {
+        board.Initialize(boardSize);
+    }
+
+    void OnValidate()
+    {
+        if (boardSize.x < 2)
+        {
+            boardSize.x = 2;
+        }
+        if (boardSize.y < 2)
+        {
+            boardSize.y = 2;
+        }
+    }
+}
---
+++ New Unity Project/Assets/GameTile.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTile : MonoBehaviour
+{
+    [SerializeField]
+    Transform arrow = default;
+
+    GameTile north, east, south, west, nextOnPath;
+    int distance;
+
+    public static void MakeEastWestNeighbors (GameTile east, GameTile west)
+    {
+        Debug.Assert(west.east == null && east.west == null, "Redefined neighbors!");
+        west.east = east;
+        east.west = west;
+    }
+
+    public static void MakeNorthSouthNeighbors(GameTile north, GameTile south)
+    {
+        Debug.Assert(south.north == null && north.south == null, "Redefined neighbors!");
+        south.north = north;
+        north.south = south;
+    }
+
+    public void ClearPath()
+    {
+        distance = int.MaxValue;
+        nextOnPath = null;
+    }
+
+    public void BecomeDestination()
+    {
+        distance = 0;
+        nextOnPath = null;
+    }
+
+    public bool HasPath => distance != int.MaxValue;
+}
